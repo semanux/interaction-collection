20b1edac269d63e4698c9a4162c18db89c95074c
irahulcse <1rahulchandra1@gmail.com>
2022-05-18 14:20:03 +0530

    Add a RadialProgress bar poc.

--- src/components/RadialProgressBar.vue
+++ src/components/RadialProgressBar.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>Hello world</div>
-  <div>making</div>
+  <div className="first">
+    <b-progress :value="value" :max="max" show-progress animated></b-progress>
+    <b-progress class="mt-2" :max="max" show-value>
+      <b-progress-bar
+        :value="value * (6 / 10)"
+        variant="success"
+      ></b-progress-bar>
+      <b-progress-bar
+        :value="value * (2.5 / 10)"
+        variant="warning"
+      ></b-progress-bar>
+      <b-progress-bar
+        :value="value * (1.5 / 10)"
+        variant="danger"
+      ></b-progress-bar>
+    </b-progress>
+
+    <b-button class="mt-3" @click="randomValue">Click me</b-button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      some: 'hello'
+      value: 45,
+      max: 100
+    }
+  },
+  methods: {
+    randomValue() {
+      this.value = Math.random() * this.max
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+#first {
+  display: flex;
+  align-items: center;
+  margin: 112;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue';
+
+// createApp(App).use(BootstrapVue).mount('#app')
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+// Since every component imports their Bootstrap functionality,
+// the following line is not necessary:
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
