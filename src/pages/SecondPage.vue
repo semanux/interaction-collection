96cd66f8fdc8402c6164ecc18bda5481df24de4d
irahulcse <1rahulchandra1@gmail.com>
2022-06-13 10:44:57 +0530

    Second Page updated

--- src/pages/SecondPage.vue
+++ src/pages/SecondPage.vue
@@ -1,13 +1,39 @@
+<script setup lang="ts">
+import { useForm, useField } from "vee-validate"
+import * as yup from "yup"
+import { useFormStore } from "../stores/formStore"
+import { useRouter } from "vue-router"
+
+const store = useFormStore()
+const router = useRouter()
+const schema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8)
+})
+
+// Create a form context with the validation schema.
+const { errors } = useForm({
+  validationSchema: schema
+})
+
+// No need to define rules for fields.
+const { value: email } = useField("email")
+const { value: password } = useField("password")
+
+const onSubmit = () => {
+  console.log("value of the email field", email.value)
+  store.setEmail(email.value as string)
+  store.setPassword(password.value as string)
+  router.push("/second")
+}
+</script>
+
 <template>
   <form @submit="onSubmit">
-    <input
-      name="email"
-      v-model="email"
-    />
+    <input v-model="email" />
     <span>{{ errors.email }}</span>
     <br>
     <input
-      name="password"
       v-model="password"
       type="password"
     />
@@ -15,46 +41,3 @@
     <button>Submit</button>
   </form>
 </template>
-
-<script>
-import { useForm, useField } from "vee-validate"
-import * as yup from "yup"
-import { useFormStore } from "../stores/formStore"
-import { useRouter } from "vue-router"
-
-export default {
-  setup() {
-    // Define a validation schema
-
-    const store = useFormStore()
-    const router = useRouter()
-    const schema = yup.object({
-      email: yup.string().required().email(),
-      password: yup.string().required().min(8)
-    })
-
-    // Create a form context with the validation schema
-    const { errors } = useForm({
-      validationSchema: schema
-    })
-
-    // No need to define rules for fields
-    const { email } = useField("email")
-    const { password } = useField("password")
-
-    const onSubmit = () => {
-      console.log("value of the value field", email)
-      store.setEmail(email)
-      store.setPassword(password)
-      router.push("/second")
-    }
-
-    return {
-      errors,
-      email,
-      password,
-      onSubmit
-    }
-  }
-}
-</script>
