cbec4f95b45135897931acf74f006e3b15d0048e
irahulcse <1rahulchandra1@gmail.com>
2022-06-13 10:17:22 +0530

    Second Page updated

--- src/pages/SecondPage.vue
+++ src/pages/SecondPage.vue
@@ -1,23 +1,60 @@
 <template>
   <form @submit="onSubmit">
-    <!-- some fields -->
+    <input
+      name="email"
+      v-model="email"
+    />
+    <span>{{ errors.email }}</span>
+    <br>
+    <input
+      name="password"
+      v-model="password"
+      type="password"
+    />
+    <span>{{ errors.password }}</span>
+    <button>Submit</button>
   </form>
 </template>
 
 <script>
-import { useForm } from 'vee-validate';
+import { useForm, useField } from "vee-validate"
+import * as yup from "yup"
+import { useFormStore } from "../stores/formStore"
+import { useRouter } from "vue-router"
 
 export default {
   setup() {
-    const { handleSubmit } = useForm();
+    // Define a validation schema
 
-    const onSubmit = handleSubmit(values => {
-      alert(JSON.stringify(values, null, 2));
-    });
+    const store = useFormStore()
+    const router = useRouter()
+    const schema = yup.object({
+      email: yup.string().required().email(),
+      password: yup.string().required().min(8)
+    })
+
+    // Create a form context with the validation schema
+    const { errors } = useForm({
+      validationSchema: schema
+    })
+
+    // No need to define rules for fields
+    const { email } = useField("email")
+    const { password } = useField("password")
+
+    const onSubmit = () => {
+      console.log("value of the value field", email)
+      store.setEmail(email)
+      store.setPassword(password)
+      router.push("/second")
+    }
 
     return {
-      onSubmit,
-    };
-  },
-};
+      errors,
+      email,
+      password,
+      onSubmit
+    }
+  }
+}
 </script>
