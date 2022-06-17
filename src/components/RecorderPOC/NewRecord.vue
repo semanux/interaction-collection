fcdb2a9bec6933e98aecbbc835bdc97384a6b859
irahulcse <1rahulchandra1@gmail.com>
2022-06-17 12:40:49 +0530

    NewRecord.vue component

--- src/components/RecorderPOC/NewRecord.vue
+++ src/components/RecorderPOC/NewRecord.vue
@@ -1,6 +1,5 @@
 <template>
 
-  <!-- omitted... -->
   <div>
     <h5>Record Audio</h5>
 
@@ -30,54 +29,43 @@
 
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      newMessageText: "",
-      loading: false,
-      messages: [],
-      newAudio: null,
-      recorder: null
+<script setup>
+var newMessageText = ""
+var loading = false
+var messages = []
+var newAudio = null
+var recorder = null
+
+const newAudioURL = () => {
+  return URL.createObjectURL(newAudio)
+}
+
+const record = async () => {
+  newAudio = null
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+  })
+
+  const options = { mimeType: "audio/webm" }
+  const recordedChunks = []
+  recorder = new MediaRecorder(stream, options)
+
+  recorder.addEventListener("dataavailable", (e) => {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data)
     }
-  },
-  computed: {
-    // omitted...
+  })
 
-    newAudioURL() {
-      return URL.createObjectURL(this.newAudio)
-    }
-  },
-  methods: {
-    async record() {
-      this.newAudio = null
+  recorder.addEventListener("stop", () => {
+    newAudio = new Blob(recordedChunks)
+    console.log(newAudio)
+  })
 
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false
-      })
-
-      const options = { mimeType: "audio/webm" }
-      const recordedChunks = []
-      this.recorder = new MediaRecorder(stream, options)
-
-      this.recorder.addEventListener("dataavailable", (e) => {
-        if (e.data.size > 0) {
-          recordedChunks.push(e.data)
-        }
-      })
-
-      this.recorder.addEventListener("stop", () => {
-        this.newAudio = new Blob(recordedChunks)
-        console.log(this.newAudio)
-      })
-
-      this.recorder.start()
-    },
-    async stop() {
-      this.recorder.stop()
-      this.recorder = null
-    }
-  }
+  recorder.start()
+}
+const stop = async () => {
+  recorder.stop()
+  recorder = null
 }
 </script>
