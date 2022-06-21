5210210047067564aa2ecedcdaba5bda7e76cffd
irahulcse <1rahulchandra1@gmail.com>
2022-06-21 15:22:33 +0530

    NewRecordComponent updated with Setup and Typescript

--- src/components/RecorderPOC/NewRecord.vue
+++ src/components/RecorderPOC/NewRecord.vue
@@ -2,7 +2,6 @@
 
   <div>
     <h5>Record Audio</h5>
-
     <button
       v-if="!recorder"
       @click="record()"
@@ -29,43 +28,44 @@
 
 </template>
 
-<script setup>
-var newMessageText = ""
-var loading = false
-var messages = []
-var newAudio = null
-var recorder = null
+<script setup lang="ts">
+import { ref } from "vue"
+const newMessageText = ref('')
+const loading = ref(false)
+const messages =ref([])
+const newAudio = ref(null as null || MediaSource || Blob) 
+const recorder = ref(null as null || MediaRecorder)
 
 const newAudioURL = () => {
-  return URL.createObjectURL(newAudio)
+  return URL.createObjectURL(newAudio.value)
 }
 
 const record = async () => {
-  newAudio = null
+  newAudio.value = null
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false
   })
 
   const options = { mimeType: "audio/webm" }
-  const recordedChunks = []
-  recorder = new MediaRecorder(stream, options)
+  const recordedChunks = [] as Array<any>
+  recorder.value = new MediaRecorder(stream, options)
 
-  recorder.addEventListener("dataavailable", (e) => {
+  recorder.value.addEventListener("dataavailable", (e) => {
     if (e.data.size > 0) {
       recordedChunks.push(e.data)
     }
   })
 
-  recorder.addEventListener("stop", () => {
+  recorder.value.addEventListener("stop", () => {
     newAudio = new Blob(recordedChunks)
     console.log(newAudio)
   })
 
-  recorder.start()
+  recorder.value.start()
 }
 const stop = async () => {
-  recorder.stop()
-  recorder = null
+  recorder.value.stop()
+  recorder.value = null
 }
 </script>
